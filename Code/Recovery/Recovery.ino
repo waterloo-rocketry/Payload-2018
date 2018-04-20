1815d19f8664ac6568cbfad3baa1d8bcd9a07a22
pauladam316 <pauladam316@gmail.com>
2018-04-20 16:16:14 -0400

    More recovery board code

--- Code/Recovery/Recovery.ino
+++ Code/Recovery/Recovery.ino
@@ -1,7 +1,9 @@
 #define stratRelayPin 12
 #define instrRelayPin 13
+#define slaveNum 1
 
 #include "RadioMessages.h"
+#include <Wire.h>
 
 enum RecoveryState {
   Idle,
@@ -12,13 +14,44 @@ enum RecoveryState {
 RecoveryState state;
 
 void setup() {
+	pinMode(stratSoftSerialPin, INPUT);
+	pinMode(stratRelayPin, OUTPUT);
+	pinMode(instrRelayPin, OUTPUT);
 
+	SwitchState(1);
+
+	// initialize serial and wire:
+	Wire.begin();//join i2c bus, address optional for master
+	Serial.begin(9600);
+	// reserve 200 bytes for the xbeeIn:
+	
 }
 
 void loop() {
+  String outgoingData = "";
   String xbeeData = ReadFromSerial();
-  if (xbeeData == "") return;
-  ParseReceivedMessage(xbeeData);
+  if (xbeeData != "")
+	ParseReceivedMessage(xbeeData);
+  
+  if (state == 1) {
+	
+  }
+  else if (state == 2) {
+	//read from i2C bus
+      Wire.requestFrom(slaveNum, 6); //request 6 bytes from slave device 
+      
+      while (Wire.available()) {  //slave may send less than requested
+        char temp = Wire.read();   //receive a byte as a char
+        outgoingData += temp;       // add char to data package
+      }
+      if (outgoingData == ""){
+        outgoingData = "disInstrError" //  add disabled instr arduino error to dataPackage if bus empty
+      }
+  }
+  else if (state == 3) {
+  
+  }
+
 }
 
 void ParseReceivedMessage(String Message) {
